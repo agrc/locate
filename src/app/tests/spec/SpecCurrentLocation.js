0588fd124578aa2e1f2a1e655cd64a70c336c150
Scott Davis <stdavis@utah.gov>
2015-01-08 08:21:18 -0700

    Better handling of loader in CurrentLocation
    Closes #16
    Addresses some points in #17

--- src/app/tests/spec/SpecCurrentLocation.js
+++ src/app/tests/spec/SpecCurrentLocation.js
@@ -1,10 +1,12 @@
 require([
     'app/CurrentLocation',
 
+    'dojo/dom-class',
     'dojo/dom-construct'
 ], function(
     WidgetUnderTest,
 
+    domClass,
     domConstruct
 ) {
     describe('app/CurrentLocation', function() {
@@ -28,6 +30,34 @@ require([
         describe('Sanity', function() {
             it('should create a CurrentLocation', function() {
                 expect(widget).toEqual(jasmine.any(WidgetUnderTest));
+            });
+        });
+        describe('toggleLoader', function () {
+            it('shows loader immediately on the first call but only after a delay on subsequent calls', function () {
+                widget.toggleLoader(true);
+
+                expect(domClass.contains(widget.loader, 'hidden')).toBe(false);
+
+                jasmine.clock().install();
+
+                widget.toggleLoader(false);
+                widget.toggleLoader(true);
+
+                expect(domClass.contains(widget.loader, 'hidden')).toBe(true);
+
+                jasmine.clock().tick(501);
+
+                expect(domClass.contains(widget.loader, 'hidden')).toBe(false);
+
+                widget.toggleLoader(false);
+                widget.toggleLoader(true);
+                widget.toggleLoader(false);
+
+                jasmine.clock().tick(501);
+
+                expect(domClass.contains(widget.loader, 'hidden')).toBe(true);
+
+                jasmine.clock().uninstall();
             });
         });
     });
