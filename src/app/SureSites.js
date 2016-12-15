881793924d7829f77b89bad26700dd02fcab761a
Scott Davis <stdavis@utah.gov>
2016-12-15 15:56:39 -0700

    initial fleshing out of sure sites group
    Related to #107

--- src/app/SureSites.js
+++ src/app/SureSites.js
@@ -1,15 +1,23 @@
 define([
+    'agrc/modules/Formatting',
+
     'dijit/_TemplatedMixin',
     'dijit/_WidgetBase',
 
+    'dojo/text!app/templates/SureSites.html',
     'dojo/_base/declare',
-    'dojo/text!app/templates/SureSites.html'
+    'dojo/_base/lang',
+
+    'slider'
 ], function (
+    formatting,
+
     _TemplatedMixin,
     _WidgetBase,
 
+    template,
     declare,
-    template
+    lang
 ) {
     return declare([_WidgetBase, _TemplatedMixin], {
         // description:
@@ -22,7 +30,7 @@ define([
         postCreate: function () {
             // summary:
             //      Overrides method of same name in dijit._Widget.
-            console.log('app.SureSites::postCreate', arguments);
+            console.log('app/SureSites:postCreate', arguments);
 
             this.setupConnections();
 
@@ -31,8 +39,23 @@ define([
         setupConnections: function () {
             // summary:
             //      wire events, and such
-            console.log('app.SureSites::setupConnections', arguments);
+            console.log('app/SureSites:setupConnections', arguments);
 
+            $(this.acreageSlider).on('slide', lang.hitch(this, this.onSliderChange));
+            $(this.sqftSlider).on('slide', lang.hitch(this, this.onSliderChange));
+        },
+        onSliderChange: function () {
+            // summary:
+            //      update labels from slider values
+            console.log('app/SureSites:onSliderChange', arguments);
+
+            var acreage = this.acreageSlider.value.split(',');
+            this.acreageMin.innerHTML = formatting.addCommas(acreage[0]);
+            this.acreageMax.innerHTML = formatting.addCommas(acreage[1]);
+
+            var sqft = this.sqftSlider.value.split(',');
+            this.sqftMin.innerHTML = formatting.addCommas(sqft[0]);
+            this.sqftMax.innerHTML = formatting.addCommas(sqft[1]);
         }
     });
 });
