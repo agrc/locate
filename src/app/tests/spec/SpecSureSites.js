4e4e9bc56cf230b3d07f04aa750c831303a3f61d
Scott Davis <stdavis@utah.gov>
2017-02-13 16:43:46 -0700

    wire up filter controls to sure sites layer
    Closes #107

--- src/app/tests/spec/SpecSureSites.js
+++ src/app/tests/spec/SpecSureSites.js
@@ -30,5 +30,46 @@ require([
                 expect(widget).toEqual(jasmine.any(WidgetUnderTest));
             });
         });
+        describe('updateDefQuery', function () {
+            it('builds the appropriate query', function () {
+                widget.layer = {
+                    setDefinitionExpression: jasmine.createSpy()
+                };
+
+                widget.updateDefQuery();
+
+                expect(widget.layer.setDefinitionExpression).toHaveBeenCalledWith('1 = 1');
+
+                widget.urbanChbx.checked = true;
+                var def = 'County IN (\'Salt Lake\', \'Utah\', \'Davis\', \'Weber\')';
+                widget.updateDefQuery();
+
+                expect(widget.layer.setDefinitionExpression).toHaveBeenCalledWith(def);
+
+                widget.landChbx.checked = true;
+                def = 'County IN (\'Salt Lake\', \'Utah\', \'Davis\', \'Weber\') AND Type IN (\'Land\')';
+                widget.updateDefQuery();
+
+                expect(widget.layer.setDefinitionExpression).toHaveBeenCalledWith(def);
+
+                widget.ruralChbx.checked = true;
+                def = 'Type IN (\'Land\')';
+                widget.updateDefQuery();
+
+                expect(widget.layer.setDefinitionExpression).toHaveBeenCalledWith(def);
+
+                widget.acreageSlider.value = ['1,2'];
+                def = 'Type IN (\'Land\') AND Acreage >= 1 AND Acreage <= 2';
+                widget.updateDefQuery();
+
+                expect(widget.layer.setDefinitionExpression).toHaveBeenCalledWith(def);
+
+                widget.sqftSlider.value = ['3,4'];
+                def = 'Type IN (\'Land\') AND Acreage >= 1 AND Acreage <= 2 AND Square_Footage >= 3 AND Square_Footage <= 4';
+                widget.updateDefQuery();
+
+                expect(widget.layer.setDefinitionExpression).toHaveBeenCalledWith(def);
+            });
+        });
     });
 });
