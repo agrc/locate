3dee2e63a44acf251c07628b797713281a3989c6
Scott Davis <stdavis@utah.gov>
2017-02-13 15:04:16 -0700

    toggle sure sites layer with group

--- src/app/SureSites.js
+++ src/app/SureSites.js
@@ -1,23 +1,37 @@
 define([
     'agrc/modules/Formatting',
 
+    'app/config',
+
     'dijit/_TemplatedMixin',
     'dijit/_WidgetBase',
 
     'dojo/text!app/templates/SureSites.html',
+    'dojo/topic',
     'dojo/_base/declare',
     'dojo/_base/lang',
+
+    'esri/layers/FeatureLayer',
+    'esri/renderers/SimpleRenderer',
+    'esri/symbols/PictureMarkerSymbol',
 
     'slider'
 ], function (
     formatting,
 
+    config,
+
     _TemplatedMixin,
     _WidgetBase,
 
     template,
+    topic,
     declare,
-    lang
+    lang,
+
+    FeatureLayer,
+    SimpleRenderer,
+    PictureMarkerSymbol
 ) {
     return declare([_WidgetBase, _TemplatedMixin], {
         // description:
@@ -46,6 +60,8 @@ define([
 
             $(this.acreageSlider).on('slide', lang.hitch(this, this.onSliderChange));
             $(this.sqftSlider).on('slide', lang.hitch(this, this.onSliderChange));
+            $(this.filtersContainer).on('show.bs.collapse', lang.partial(lang.hitch(this, this.toggleLayer), true));
+            $(this.filtersContainer).on('hide.bs.collapse', lang.partial(lang.hitch(this, this.toggleLayer), false));
         },
         onSliderChange: function () {
             // summary:
@@ -59,6 +75,38 @@ define([
             var sqft = this.sqftSlider.value.split(',');
             this.sqftMin.innerHTML = formatting.addCommas(sqft[0]);
             this.sqftMax.innerHTML = formatting.addCommas(sqft[1]);
+        },
+        toggleLayer: function (show) {
+            // summary:
+            //      toggles the feature layer visibility
+            // show: Boolean
+            console.log('app/SureSites:toggleLayer', arguments);
+
+            if (show && !this.layer) {
+                var markerSymbol = new PictureMarkerSymbol(
+                    'app/resources/img/markers/suresites.svg',
+                    config.markerSymbolWidth,
+                    config.markerSymbolHeight
+                );
+                markerSymbol.setOffset(0, config.markerSymbolHeight/2);
+                this.layer = new FeatureLayer(config.urls.mapService + '/' + config.sureSitesLayerIndex, {
+                    outFields: ['*']
+                });
+                this.layer.setRenderer(new SimpleRenderer(markerSymbol));
+                this.layer.on('load', function () {
+                    this.layer.on('mouse-over', function () {
+                        // var g = evt.graphic;
+                        // popup.setContent(g.attributes[this.layer.displayField]);
+                        // popup.show(g.geometry);
+                    });
+                    this.layer.on('mouse-out', function () {
+                        // popup.hide();
+                    });
+                }.bind(this));
+                topic.publish(config.topics.addLayer, this.layer);
+            }
+
+            this.layer.setVisibility(show);
         }
     });
 });
